791333f59876b52d0cd5dc712740f4403e7883c3
gbabeau <gbabeau@student.42.fr>
2023-03-10 18:52:31 +0100

    envoie jusau'qu juras

--- main.py
+++ main.py
@@ -32,67 +32,72 @@ class linear_regression:
         self.teta1 = 0.0
         self.error = []
 
-    def  estimateabscisse(self, milleage):
-        return (self.teta0 +  (self.teta1* milleage))
+    def  estimate_abscisse(self, x):
+        return (self.teta0 + (self.teta1 * x))
             
-    def  estimateabscisse_absolue(self, milleage):
-        return (self.nteta0 +  (self.nteta1* milleage))
+    def  estimate_abscisse_absolue(self, x):
+        return (self.nteta0 + (self.nteta1 * x))
 
-    def estimatetmpteta0(self):
+    def estimate_tmpteta0(self):
         tmp = 0
         for index in range(len(self.ordonnee)):
-            tmp += self.estimateabscisse(self.ordonnee[index]) - self.abscisse[index]
+            tmp += self.estimate_abscisse(self.ordonnee[index]) - self.abscisse[index]
         return (tmp * self.lear / len(self.ordonnee))
 
-    def estimatetmpteta1(self):
+    def estimate_tmpteta1(self):
         tmp = 0
         for index in range(len(self.ordonnee)):
-            tmp += (self.estimateabscisse(self.ordonnee[index]) - self.abscisse[index]) * float(self.ordonnee[index])
+            tmp += (self.estimate_abscisse(self.ordonnee[index]) - self.abscisse[index]) * float(self.ordonnee[index])
         return (tmp * self.lear / len(self.ordonnee))
 
     def error_abscisse(self):
         tmp = 0
         for index in range(len(self.ordonnee)):
-            tmp += (self.estimateabscisse(self.ordonnee[index]) - self.abscisse[index]) ** 2
+            tmp += (self.estimate_abscisse(self.ordonnee[index]) - self.abscisse[index]) ** 2
         return tmp / len(self.ordonnee)
 
     def init_valeur_relative(self):
         for index in range(len(self.ordonnee_relative)):
             self.ordonnee.append((self.ordonnee_relative[index] - self.ordonnee_relative_limite[0])/ (self.ordonnee_relative_limite[1] - self.ordonnee_relative_limite[0]))
             self.abscisse.append((self.abscisse_relative[index] - self.abscisse_relative_limite[0])/ (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0]))
-            print(self.ordonnee[-1], self.abscisse[-1])
 
     def show_relative(self):
+        plt.figure("ft_linear_regression")
         plt.scatter(self.ordonnee, self.abscisse)
-        plt.plot([min(self.ordonnee), max(self.ordonnee)],[self.estimateabscisse(min(self.ordonnee)), self.estimateabscisse(max(self.ordonnee))], color = 'green', linestyle = 'solid')
+        plt.plot([min(self.ordonnee), max(self.ordonnee)],[self.estimate_abscisse(min(self.ordonnee)), self.estimate_abscisse(max(self.ordonnee))], color = 'green', linestyle = 'solid')
         plt.ylabel(self.index[0])
         plt.xlabel(self.index[1])
+        plt.title("relative")
         plt.show()
 
 
     def show_absolue(self):
+        plt.figure("ft_linear_regression")
         plt.scatter(self.ordonnee_relative, self.abscisse_relative)
-        plt.plot([0,  self.ordonnee_relative_limite[1]],[self.estimateabscisse_absolue(0), \
-                            self.estimateabscisse_absolue(self.ordonnee_relative_limite[1])], \
+        plt.plot([0,  self.ordonnee_relative_limite[1]],[self.estimate_abscisse_absolue(0), \
+                            self.estimate_abscisse_absolue(self.ordonnee_relative_limite[1])], \
                             color = 'green', linestyle = 'solid')
         plt.ylabel(self.index[0])
         plt.xlabel(self.index[1])
+        plt.title("absolue")
         plt.show()
 
     def show_error(self):
+        plt.figure("ft_linear_regression")
         plt.plot(self.error)
+        plt.title("error")
         plt.show()
 
     def estimate_teta(self):
         tmp0 = 0
         tmp1 = 0
         while (len(self.error) < 2 or  self.error[-2] - self.error[-1] != 0.0) and len(self.error) != 1000000:
-            tmp0 -= self.estimatetmpteta0()
-            tmp1 -= self.estimatetmpteta1()
+            tmp0 -= self.estimate_tmpteta0()
+            tmp1 -= self.estimate_tmpteta1()
             self.error.append(self.error_abscisse())
             self.teta0 = tmp0
             self.teta1 = tmp1
-        self.nteta1 = self.teta1 / ((self.ordonnee_relative_limite[1]- self.ordonnee_relative_limite[0])/ (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0]))
+        self.nteta1 = self.teta1 / ((self.ordonnee_relative_limite[1] - self.ordonnee_relative_limite[0]) / (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0]))
         self.nteta0 = (self.teta0 * (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0])) + self.abscisse_relative_limite[0] - self.nteta1 * self.ordonnee_relative_limite[0]
         self.error.append(self.error_abscisse())
     
@@ -104,7 +109,7 @@ line.estimate_teta()
 line.show_relative()
 line.show_absolue()
 line.show_error()
-fin = open("teta.csv","w")
+fin = open("ft_linear_regression.csv","w")
 fin.write(line.index[0]+","+line.index[1]+"\n")
 valeur = "%f,%f\n" % (line.nteta0,line.nteta1)
 fin.write(valeur)
--- main2.py
+++ main2.py
@@ -1,13 +1,10 @@
 import array as arr
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
 
 def  estimateprice(milleage, teta0, teta1):
     #print(teta0 +  (teta1* float(milleage)))
     print("estimate price : ",(teta0 +  (teta1 * float(milleage))))
 
-fichier = open("teta.csv", "r")
+fichier = open("ft_linear_regression.csv", "r")
 tmp = (fichier.read()).split()
 index = tmp[0].split(",")
 valeur = tmp[1].split(",")
