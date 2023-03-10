12548544b9192a4a044879708397b7992804fb41
gbabeau <gbabeau@student.42.fr>
2023-03-10 18:24:45 +0100

    envoie

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import array as arr
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy import stats
+
 
 class linear_regression:
-    def _init_(self, data):
-        for index in range(len(las)-1):
-            v = las[index+1].split(',')
-            self.l1.append(int(v[0]))
-            self.l2.append(int(v[1]))
-        self.l1li[0] = min(l1)
-        self.l2li[0] = min(l2)
-        self.l1li[1] = max(l1)
-        self.l2li[1] = max(l2)
-        self.index = las[0].split(',')
-        self.l1= []
-        self.l2 = []
-        self.km = [] 
-        self.price = []
+    ordonnee_relative = []
+    abscisse_relative = []
+    ordonnee = [] 
+    abscisse = []
+    lear = 0.5
+    teta0 = 0.0
+    teta1 = 0.0
+    error = []
+    ordonnee_relative_limite = [0,0]
+    abscisse_relative_limite = [0,0]
+    def __init__(self, data):
+        for index in range(len(data)-1):
+            v = data[index+1].split(',')
+            self.ordonnee_relative.append(int(v[0]))
+            self.abscisse_relative.append(int(v[1]))
+        self.ordonnee_relative_limite[0] = min(self.ordonnee_relative)
+        self.abscisse_relative_limite[0] = min(self.abscisse_relative)
+        self.ordonnee_relative_limite[1] = max(self.ordonnee_relative)
+        self.abscisse_relative_limite[1] = max(self.abscisse_relative)
+        self.index = data[0].split(',')
+        self.ordonnee = [] 
+        self.abscisse = []
         self.lear = 0.5
         self.tmp0 = 0.0
         self.tmp1 = 0.0
@@ -25,78 +32,80 @@ class linear_regression:
         self.teta1 = 0.0
         self.error = []
 
-    def  estimateprice(teta0, teta1, milleage):
-        return (teta0 +  (teta1* milleage))
+    def  estimateabscisse(self, milleage):
+        return (self.teta0 +  (self.teta1* milleage))
+            
+    def  estimateabscisse_absolue(self, milleage):
+        return (self.nteta0 +  (self.nteta1* milleage))
 
-    def estimatetmpteta0(teta0,teta1, km, price,lear):
+    def estimatetmpteta0(self):
         tmp = 0
-        for index in range(len(km)):
-            tmp += estimateprice(teta0, teta1, km[index]) - price[index]
-        return (tmp * lear / len(self.km))
+        for index in range(len(self.ordonnee)):
+            tmp += self.estimateabscisse(self.ordonnee[index]) - self.abscisse[index]
+        return (tmp * self.lear / len(self.ordonnee))
 
-    def estimatetmpteta1(teta0,teta1, km, price,lear):
+    def estimatetmpteta1(self):
         tmp = 0
-        for index in range(len(self.km)):
-            tmp += (estimateprice(self.teta0, teta1, self.km[index]) - self.price[index]) * float(self.km[index])
-    return (tmp * self.lear / len(self.km))
+        for index in range(len(self.ordonnee)):
+            tmp += (self.estimateabscisse(self.ordonnee[index]) - self.abscisse[index]) * float(self.ordonnee[index])
+        return (tmp * self.lear / len(self.ordonnee))
 
-    def error_price(teta0, teta1, km, price,lear):
+    def error_abscisse(self):
         tmp = 0
-        for index in range(len(km)):
-            tmp += (estimateprice(teta0, teta1, km[index]) - price[index]) ** 2
-        return tmp / len(km)
+        for index in range(len(self.ordonnee)):
+            tmp += (self.estimateabscisse(self.ordonnee[index]) - self.abscisse[index]) ** 2
+        return tmp / len(self.ordonnee)
 
+    def init_valeur_relative(self):
+        for index in range(len(self.ordonnee_relative)):
+            self.ordonnee.append((self.ordonnee_relative[index] - self.ordonnee_relative_limite[0])/ (self.ordonnee_relative_limite[1] - self.ordonnee_relative_limite[0]))
+            self.abscisse.append((self.abscisse_relative[index] - self.abscisse_relative_limite[0])/ (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0]))
+            print(self.ordonnee[-1], self.abscisse[-1])
 
-    def init_valeur_relative(l1, l2, l2li, l1li):
-        for index in range(len(l1)):
-            km.append((l1[index] - l1li[0])/ (l1li[1] - l1li[0]))
-            price.append((l2[index] - l2li[0])/ (l2li[1] - l2li[0]))
-
-    def show_relative():
-        plt.scatter(km,price)
-        plt.plot([min(km), max(km)],[estimateprice(teta0, teta1, min(km)), estimateprice(teta0, teta1, max(km))], color = 'green', linestyle = 'solid')
-        plt.ylabel(index[0])
-        plt.xlabel(index[1])
+    def show_relative(self):
+        plt.scatter(self.ordonnee, self.abscisse)
+        plt.plot([min(self.ordonnee), max(self.ordonnee)],[self.estimateabscisse(min(self.ordonnee)), self.estimateabscisse(max(self.ordonnee))], color = 'green', linestyle = 'solid')
+        plt.ylabel(self.index[0])
+        plt.xlabel(self.index[1])
         plt.show()
 
 
-    def show_absolue():
-        plt.scatter(l1,l2)
-        plt.plot([0,  l1li[1]],[estimateprice(nteta0 , nteta1, 0), \
-                            estimateprice(nteta0, nteta1, l1li[1])], \
+    def show_absolue(self):
+        plt.scatter(self.ordonnee_relative, self.abscisse_relative)
+        plt.plot([0,  self.ordonnee_relative_limite[1]],[self.estimateabscisse_absolue(0), \
+                            self.estimateabscisse_absolue(self.ordonnee_relative_limite[1])], \
                             color = 'green', linestyle = 'solid')
-        plt.ylabel(index[0])
-        plt.xlabel(index[1])
+        plt.ylabel(self.index[0])
+        plt.xlabel(self.index[1])
         plt.show()
 
-    def show_error():
-        plt.plot(error)
+    def show_error(self):
+        plt.plot(self.error)
         plt.show()
 
-    def estimate_teta(teta0, teta1):
+    def estimate_teta(self):
         tmp0 = 0
         tmp1 = 0
-        while (len(error) < 2 or  error[-2] - error[-1] != 0.0) and len(error) != 1000000:
-            tmp0 -= estimatetmpteta0(teta0, teta1, km, price,lear)
-            tmp1 -= estimatetmpteta1(teta0, teta1, km, price,lear)
-            error.append(error_price(teta0, teta1, km, price,lear))
-            teta0 = tmp0
-            teta1 = tmp1
-    nteta1 = teta1 / ((l1li[1]- l1li[0])/ (l2li[1] - l2li[0]))
-    nteta0 = (teta0 * (l2li[1] - l2li[0])) + l2li[0] - nteta1 * l1li[0]
-    error.append(error_price(teta0, teta1, km, price,lear))
-    return nteta0, nteta1, teta0, teta1
+        while (len(self.error) < 2 or  self.error[-2] - self.error[-1] != 0.0) and len(self.error) != 1000000:
+            tmp0 -= self.estimatetmpteta0()
+            tmp1 -= self.estimatetmpteta1()
+            self.error.append(self.error_abscisse())
+            self.teta0 = tmp0
+            self.teta1 = tmp1
+        self.nteta1 = self.teta1 / ((self.ordonnee_relative_limite[1]- self.ordonnee_relative_limite[0])/ (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0]))
+        self.nteta0 = (self.teta0 * (self.abscisse_relative_limite[1] - self.abscisse_relative_limite[0])) + self.abscisse_relative_limite[0] - self.nteta1 * self.ordonnee_relative_limite[0]
+        self.error.append(self.error_abscisse())
     
 data = open("data.csv", "r")
 line = linear_regression((data.read()).split())
 data.close()
-init_valeur_relative(l1, l2, l2li, l1li)
-nteta0 , nteta1, teta0 , teta1 = estimate_teta(teta0, teta1)
-show_relative()
-show_absolue()
-show_error()
+line.init_valeur_relative()
+line.estimate_teta()
+line.show_relative()
+line.show_absolue()
+line.show_error()
 fin = open("teta.csv","w")
-fin.write("teta0,teat1\n")
-valeur = "%f,%f\n" % (nteta0,nteta1)
+fin.write(line.index[0]+","+line.index[1]+"\n")
+valeur = "%f,%f\n" % (line.nteta0,line.nteta1)
 fin.write(valeur)
 fin.close()
--- main2.py
+++ main2.py
@@ -8,5 +8,7 @@ def  estimateprice(milleage, teta0, teta1):
     print("estimate price : ",(teta0 +  (teta1 * float(milleage))))
 
 fichier = open("teta.csv", "r")
-valeur = (fichier.read()).split()[1].split(",")
-estimateprice(input("enter valeur estimate:") , float(valeur[0]),float(valeur[1]))
+tmp = (fichier.read()).split()
+index = tmp[0].split(",")
+valeur = tmp[1].split(",")
+estimateprice(input("numbers of "+ index[0]+" : ") , float(valeur[0]),float(valeur[1]))
